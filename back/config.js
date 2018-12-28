631abf6100ce41dc607ed1fc244b57715c890742
gabormuranyi-planorama <gabor.muranyi@planorama.com>
2018-12-28 14:44:00 +0100

    polish

--- back/config.js
+++ back/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': process.env.SESSION_SECRET || 'valamivalahol',
-    'gmailPass': process.env.GMAIL_PASS || 'jelszo0500'
+    'gmailPass': process.env.GMAIL_PASS || 'zek'
 
 };
